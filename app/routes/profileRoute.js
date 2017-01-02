817b937ee72db6665db42058eeb061d890ad0b3b
sunny sharma <sv.sunnyshrma@gmail.com>
2017-01-02 15:35:11 +0530

    changes

--- app/routes/profileRoute.js
+++ app/routes/profileRoute.js
@@ -1,11 +1,28 @@
-module.exports = function (app,express) {
-	let router = express.Router();
-	let ProfileController = require('../../app/controllers/ProfileController');
-	let authorization = require('../../app/middlewares/user_TokenAuth');
-	let adminAuthorization = require('../../app/middlewares/admin_TokenAuth');
+module.exports = function(app, express) {
+    let mime = require('mime');
+    let multer = require('multer');
+    let path = require('path');
+    let router = express.Router();
+    let ProfileController = require('../../app/controllers/ProfileController');
+    let authorization = require('../../app/middlewares/user_TokenAuth');
+    let adminAuthorization = require('../../app/middlewares/admin_TokenAuth');
+    let storage = multer.diskStorage({
+        destination: function(req, res, cb) {
+            cb(null, path.join(__dirname, '../../public/images/profile-images'));
+        },
+        filename: function(req, file, cb) {
+            cb(null, Date.now() + '.' + mime.extension(file.mimetype));
+        }
+    });
 
-	// profile routes
-	router.get('/:id',authorization,ProfileController.getUserProfileDetails);
 
-	app.use('/profile', router);
+    let upload = multer({ storage: storage });
+
+    // profile routes
+    router.get('/:id', authorization, ProfileController.getUserProfileDetails);
+    router.post('/uploadIMage',authorization,upload.single('image'), ProfileController.uploadImage);
+    router.post('/create',authorization,ProfileController.createProfile);
+    router.post('/update/:id',authorization,ProfileController.updateProfile);
+
+    app.use('/profile', router);
 }
